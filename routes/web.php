59d309a359faff9e109e4c50fee0563218474bca
mohammed-mostafa5 <m5z.mohammed@gmail.com>
2021-09-09 16:12:57 +0200

    Add Command in routes

--- routes/web.php
+++ routes/web.php
@@ -1,20 +1,15 @@
 <?php
 
-
+use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+// Custom Routs
 
+// Route::get('/mfs', function () {
+//     Artisan::call('migrate:fresh --seed');
+//     return 'done';
+// });
 
 
 Route::get('generator_builder', '\InfyOm\GeneratorBuilder\Controllers\GeneratorBuilderController@builder')->name('io_generator_builder');
