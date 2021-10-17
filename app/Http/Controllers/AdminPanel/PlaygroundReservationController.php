31999a2daf875f4f8237b04773254baa5c72b236
mohammed-mostafa5 <m5z.mohammed@gmail.com>
2021-10-17 10:44:32 +0200

    Tickets controller clean

--- app/Http/Controllers/AdminPanel/PlaygroundReservationController.php
+++ app/Http/Controllers/AdminPanel/PlaygroundReservationController.php
@@ -20,13 +20,7 @@ class PlaygroundReservationController extends AppBaseController
         $this->playgroundReservationRepository = $playgroundReservationRepo;
     }
 
-    /**
-     * Display a listing of the PlaygroundReservation.
-     *
-     * @param Request $request
-     *
-     * @return Response
-     */
+
     public function index(Request $request)
     {
         $playgroundReservations = $this->playgroundReservationRepository->all();
@@ -35,41 +29,6 @@ class PlaygroundReservationController extends AppBaseController
             ->with('playgroundReservations', $playgroundReservations);
     }
 
-    /**
-     * Show the form for creating a new PlaygroundReservation.
-     *
-     * @return Response
-     */
-    public function create()
-    {
-        return view('adminPanel.playground_reservations.create');
-    }
-
-    /**
-     * Store a newly created PlaygroundReservation in storage.
-     *
-     * @param CreatePlaygroundReservationRequest $request
-     *
-     * @return Response
-     */
-    public function store(CreatePlaygroundReservationRequest $request)
-    {
-        $input = $request->all();
-
-        $playgroundReservation = $this->playgroundReservationRepository->create($input);
-
-        Flash::success(__('messages.saved', ['model' => __('models/playgroundReservations.singular')]));
-
-        return redirect(route('adminPanel.playgroundReservations.index'));
-    }
-
-    /**
-     * Display the specified PlaygroundReservation.
-     *
-     * @param int $id
-     *
-     * @return Response
-     */
     public function show($id)
     {
         $playgroundReservation = $this->playgroundReservationRepository->find($id);
@@ -83,74 +42,4 @@ class PlaygroundReservationController extends AppBaseController
         return view('adminPanel.playground_reservations.show')->with('playgroundReservation', $playgroundReservation);
     }
 
-    /**
-     * Show the form for editing the specified PlaygroundReservation.
-     *
-     * @param int $id
-     *
-     * @return Response
-     */
-    public function edit($id)
-    {
-        $playgroundReservation = $this->playgroundReservationRepository->find($id);
-
-        if (empty($playgroundReservation)) {
-            Flash::error(__('messages.not_found', ['model' => __('models/playgroundReservations.singular')]));
-
-            return redirect(route('adminPanel.playgroundReservations.index'));
-        }
-
-        return view('adminPanel.playground_reservations.edit')->with('playgroundReservation', $playgroundReservation);
-    }
-
-    /**
-     * Update the specified PlaygroundReservation in storage.
-     *
-     * @param int $id
-     * @param UpdatePlaygroundReservationRequest $request
-     *
-     * @return Response
-     */
-    public function update($id, UpdatePlaygroundReservationRequest $request)
-    {
-        $playgroundReservation = $this->playgroundReservationRepository->find($id);
-
-        if (empty($playgroundReservation)) {
-            Flash::error(__('messages.not_found', ['model' => __('models/playgroundReservations.singular')]));
-
-            return redirect(route('adminPanel.playgroundReservations.index'));
-        }
-
-        $playgroundReservation = $this->playgroundReservationRepository->update($request->all(), $id);
-
-        Flash::success(__('messages.updated', ['model' => __('models/playgroundReservations.singular')]));
-
-        return redirect(route('adminPanel.playgroundReservations.index'));
-    }
-
-    /**
-     * Remove the specified PlaygroundReservation from storage.
-     *
-     * @param int $id
-     *
-     * @throws \Exception
-     *
-     * @return Response
-     */
-    public function destroy($id)
-    {
-        $playgroundReservation = $this->playgroundReservationRepository->find($id);
-
-        if (empty($playgroundReservation)) {
-            Flash::error(__('messages.not_found', ['model' => __('models/playgroundReservations.singular')]));
-
-            return redirect(route('adminPanel.playgroundReservations.index'));
-        }
-
-        $this->playgroundReservationRepository->delete($id);
-
-        Flash::success(__('messages.deleted', ['model' => __('models/playgroundReservations.singular')]));
-
-        return redirect(route('adminPanel.playgroundReservations.index'));
-    }
 }
--- app/Http/Controllers/AdminPanel/TicketReservationController.php
+++ app/Http/Controllers/AdminPanel/TicketReservationController.php
@@ -20,13 +20,7 @@ class TicketReservationController extends AppBaseController
         $this->ticketReservationRepository = $ticketReservationRepo;
     }
 
-    /**
-     * Display a listing of the TicketReservation.
-     *
-     * @param Request $request
-     *
-     * @return Response
-     */
+
     public function index(Request $request)
     {
         $ticketReservations = $this->ticketReservationRepository->all();
@@ -35,41 +29,7 @@ class TicketReservationController extends AppBaseController
             ->with('ticketReservations', $ticketReservations);
     }
 
-    /**
-     * Show the form for creating a new TicketReservation.
-     *
-     * @return Response
-     */
-    public function create()
-    {
-        return view('adminPanel.ticket_reservations.create');
-    }
 
-    /**
-     * Store a newly created TicketReservation in storage.
-     *
-     * @param CreateTicketReservationRequest $request
-     *
-     * @return Response
-     */
-    public function store(CreateTicketReservationRequest $request)
-    {
-        $input = $request->all();
-
-        $ticketReservation = $this->ticketReservationRepository->create($input);
-
-        Flash::success(__('messages.saved', ['model' => __('models/ticketReservations.singular')]));
-
-        return redirect(route('adminPanel.ticketReservations.index'));
-    }
-
-    /**
-     * Display the specified TicketReservation.
-     *
-     * @param int $id
-     *
-     * @return Response
-     */
     public function show($id)
     {
         $ticketReservation = $this->ticketReservationRepository->find($id);
@@ -83,74 +43,5 @@ class TicketReservationController extends AppBaseController
         return view('adminPanel.ticket_reservations.show')->with('ticketReservation', $ticketReservation);
     }
 
-    /**
-     * Show the form for editing the specified TicketReservation.
-     *
-     * @param int $id
-     *
-     * @return Response
-     */
-    public function edit($id)
-    {
-        $ticketReservation = $this->ticketReservationRepository->find($id);
 
-        if (empty($ticketReservation)) {
-            Flash::error(__('messages.not_found', ['model' => __('models/ticketReservations.singular')]));
-
-            return redirect(route('adminPanel.ticketReservations.index'));
-        }
-
-        return view('adminPanel.ticket_reservations.edit')->with('ticketReservation', $ticketReservation);
-    }
-
-    /**
-     * Update the specified TicketReservation in storage.
-     *
-     * @param int $id
-     * @param UpdateTicketReservationRequest $request
-     *
-     * @return Response
-     */
-    public function update($id, UpdateTicketReservationRequest $request)
-    {
-        $ticketReservation = $this->ticketReservationRepository->find($id);
-
-        if (empty($ticketReservation)) {
-            Flash::error(__('messages.not_found', ['model' => __('models/ticketReservations.singular')]));
-
-            return redirect(route('adminPanel.ticketReservations.index'));
-        }
-
-        $ticketReservation = $this->ticketReservationRepository->update($request->all(), $id);
-
-        Flash::success(__('messages.updated', ['model' => __('models/ticketReservations.singular')]));
-
-        return redirect(route('adminPanel.ticketReservations.index'));
-    }
-
-    /**
-     * Remove the specified TicketReservation from storage.
-     *
-     * @param int $id
-     *
-     * @throws \Exception
-     *
-     * @return Response
-     */
-    public function destroy($id)
-    {
-        $ticketReservation = $this->ticketReservationRepository->find($id);
-
-        if (empty($ticketReservation)) {
-            Flash::error(__('messages.not_found', ['model' => __('models/ticketReservations.singular')]));
-
-            return redirect(route('adminPanel.ticketReservations.index'));
-        }
-
-        $this->ticketReservationRepository->delete($id);
-
-        Flash::success(__('messages.deleted', ['model' => __('models/ticketReservations.singular')]));
-
-        return redirect(route('adminPanel.ticketReservations.index'));
-    }
 }
