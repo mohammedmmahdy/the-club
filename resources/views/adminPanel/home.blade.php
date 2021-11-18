fce38a9711029adf86a9840d8b55d904b6bd5111
mohammed-mostafa5 <m5z.mohammed@gmail.com>
2021-11-18 16:59:29 +0200

    test webhook

--- resources/views/adminPanel/home.blade.php
+++ resources/views/adminPanel/home.blade.php
@@ -12,6 +12,7 @@
 		<div class=" container ">
 			<div class="row">
                 <div class="col-lg-12">
+                    {{--  --}}
                     <!--begin::Card-->
                     {{-- <div class="card card-custom gutter-b example example-compact">
                         <div class="card-header">
