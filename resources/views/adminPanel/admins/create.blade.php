b00c9489eb5b054a429f74d903fe6ebede8b9a7b
mohammed-mostafa5 <m5z.mohammed@gmail.com>
2022-01-11 11:15:49 +0200

    admin crud fix

--- resources/views/adminPanel/admins/create.blade.php
+++ resources/views/adminPanel/admins/create.blade.php
@@ -1,34 +1,35 @@
 @extends('adminPanel.layouts.app')
 
-@section('title', __('models/admins.plural') . ' ' . $settings->where('key', 'title_prefix')->first()->value)
-
-@section('content')
-    <ol class="breadcrumb">
-      <li class="breadcrumb-item">
+@section('breadcrumb')
+<ul class="breadcrumb breadcrumb-transparent breadcrumb-dot font-weight-bold p-0 my-2 font-size-sm">
+    <li class="breadcrumb-item">
          <a href="{!! route('adminPanel.admins.index') !!}">@lang('models/admins.singular')</a>
-      </li>
-      <li class="breadcrumb-item active">@lang('crud.add_new')</li>
-    </ol>
-     <div class="container-fluid">
-          <div class="animated fadeIn">
-                @include('coreui-templates::common.errors')
-                <div class="row">
-                    <div class="col-lg-12">
-                        <div class="card">
-                            <div class="card-header">
-                                <i class="fa fa-plus-square-o fa-lg"></i>
-                                <strong>Create @lang('models/admins.singular')</strong>
-                            </div>
-                            <div class="card-body">
-                                {!! Form::open(['route' => 'adminPanel.admins.store']) !!}
-
-                                   @include('adminPanel.admins.fields')
-
-                                {!! Form::close() !!}
-                            </div>
+    </li>
+    <li class="breadcrumb-item active">@lang('crud.add_new')</li>
+</ul>
+@endsection
+@section('content')
+    <div class="d-flex flex-column-fluid">
+        <!--begin::Container-->
+        <div class=" container ">
+            @include('coreui-templates::common.errors')
+            <div class="row">
+                <div class="col-lg-12">
+                    <!--begin::Card-->
+                    <div class="card card-custom gutter-b example example-compact">
+                        <div class="card-header">
+                            <h3 class="card-title">Create @lang('models/admins.singular')</h3>
+                        </div>
+                        <div class="card-body">
+                            {!! Form::open(['route' => 'adminPanel.admins.store', 'files' => true]) !!}
+                                @include('adminPanel.admins.fields')
+                            {!! Form::close() !!}
                         </div>
                     </div>
+                    <!--end::Card-->
                 </div>
-           </div>
+            </div>
+        </div>
+        <!--end::Container-->
     </div>
 @endsection
--- resources/views/adminPanel/admins/edit.blade.php
+++ resources/views/adminPanel/admins/edit.blade.php
@@ -1,34 +1,35 @@
 @extends('adminPanel.layouts.app')
 
-@section('title', __('models/admins.plural') . ' ' . $settings->where('key', 'title_prefix')->first()->value)
-
+@section('breadcrumb')
+<ul class="breadcrumb breadcrumb-transparent breadcrumb-dot font-weight-bold p-0 my-2 font-size-sm">
+    <li class="breadcrumb-item">
+        <a href="{!! route('adminPanel.admins.index') !!}">@lang('models/admins.singular')</a>
+    </li>
+    <li class="breadcrumb-item active">@lang('crud.edit')</li>
+</ul>
+@endsection
 @section('content')
-    <ol class="breadcrumb">
-          <li class="breadcrumb-item">
-             <a href="{!! route('adminPanel.admins.index') !!}">@lang('models/admins.singular')</a>
-          </li>
-          <li class="breadcrumb-item active">@lang('crud.edit')</li>
-        </ol>
-    <div class="container-fluid">
-         <div class="animated fadeIn">
-             @include('coreui-templates::common.errors')
-             <div class="row">
-                 <div class="col-lg-12">
-                      <div class="card">
-                          <div class="card-header">
-                              <i class="fa fa-edit fa-lg"></i>
-                              <strong>Edit @lang('models/admins.singular')</strong>
-                          </div>
-                          <div class="card-body">
-                              {!! Form::model($admin, ['route' => ['adminPanel.admins.update', $admin->id], 'method' => 'patch']) !!}
-
+    <div class="d-flex flex-column-fluid">
+        <!--begin::Container-->
+        <div class=" container ">
+            @include('coreui-templates::common.errors')
+            <div class="row">
+                <div class="col-lg-12">
+                    <!--begin::Card-->
+                    <div class="card card-custom gutter-b example example-compact">
+                        <div class="card-header">
+                            <h3 class="card-title">Edit @lang('models/admins.singular')</h3>
+                        </div>
+                        <div class="card-body">
+                            {!! Form::model($offer, ['route' => ['adminPanel.admins.update', $offer->id], 'method' => 'patch', 'files' => true]) !!}
                               @include('adminPanel.admins.fields')
-
-                              {!! Form::close() !!}
-                            </div>
+                            {!! Form::close() !!}
                         </div>
                     </div>
+                    <!--end::Card-->
                 </div>
-         </div>
+            </div>
+        </div>
+        <!--end::Container-->
     </div>
 @endsection
